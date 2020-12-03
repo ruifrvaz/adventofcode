fd3324740195b3c1266ec8695d2d9b5d745e5f35
Rui Vaz <dig.int.sys@gmail.com>
2020-12-03 18:36:22 +0100

    setting up day 3

--- Program.cs
+++ Program.cs
@@ -11,18 +11,28 @@ namespace AdventOfCode
             Console.WriteLine("Starting Advent Of Code challenge!");
             Console.WriteLine("<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>");
 
+            // Day 1
             var answerDay1Part1 = Puzzle1.SolveFirstPartOfPuzzle();
             var answerDay1Part2 = Puzzle1.SolveSecondPartOfPuzzle();
 
+            // Day 2
             var answerDay2Part1 = Puzzle2.SolveFirstPartOfPuzzle();
             var answerDay2Part2 = Puzzle2.SolveSecondPartOfPuzzle();
+
+            // Day 3
+            var answerDay3Part1 = Puzzle3.SolveFirstPartOfPuzzle();
+            var answerDay3Part2 = Puzzle3.SolveSecondPartOfPuzzle();
 
             Task.WaitAll(answerDay1Part1, answerDay1Part2, answerDay2Part1);
 
             Console.WriteLine($"The answer to Day 1 part 1 puzzle is: {answerDay1Part1.Result}.");
             Console.WriteLine($"The answer to Day 1 part 2 puzzle is: {answerDay1Part2.Result}.");
-            Console.WriteLine($"The answer to Day 1 part 2 puzzle is: {answerDay2Part1.Result}.");
-            Console.WriteLine($"The answer to Day 1 part 2 puzzle is: {answerDay2Part2.Result}.");
+            Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            Console.WriteLine($"The answer to Day 2 part 2 puzzle is: {answerDay2Part1.Result}.");
+            Console.WriteLine($"The answer to Day 2 part 2 puzzle is: {answerDay2Part2.Result}.");
+            Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            Console.WriteLine($"The answer to Day 3 part 2 puzzle is: {answerDay3Part1.Result}.");
+            Console.WriteLine($"The answer to Day 3 part 2 puzzle is: {answerDay3Part2.Result}.");
         }
     }
 }
