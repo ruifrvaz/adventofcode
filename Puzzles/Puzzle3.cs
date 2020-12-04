966ad3e72cd8ff5154a0788f769ca6be95f824ea
Rui Vaz <dig.int.sys@gmail.com>
2020-12-04 19:50:30 +0100

    solved puzzle 3

--- Puzzles/Puzzle3.cs
+++ Puzzles/Puzzle3.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AdventOfCode.Helpers;
@@ -7,20 +8,15 @@ namespace AdventOfCode.Puzzles
 {
     public class Puzzle3
     {
-        private static string FILEPATH = "Files/puzzlelist2.txt";
+        private static string FILEPATH = "Files/puzzlelist3.txt";
 
         public async static Task<int> SolveFirstPartOfPuzzle()
         {
-            var numberOfValidPasswords = 0;
             try
             {
-                var passwordList = await FileHelper.LoadPuzzleList(FILEPATH);
-                foreach (var password in passwordList)
-                {
-                   
-                }
-                
-                return numberOfValidPasswords;
+                var itineraries = await FileHelper.LoadPuzzleList(FILEPATH);
+                var slope = 3;
+                return CountNumberOfTrees(slope, 1, itineraries);
             }
             catch (Exception ex) 
             {
@@ -32,16 +28,21 @@ namespace AdventOfCode.Puzzles
 
         public async static Task<int> SolveSecondPartOfPuzzle()
         {
-            var numberOfValidPasswords = 0;
+            var itineraries = await FileHelper.LoadPuzzleList(FILEPATH);
             try
             {
-                var passwordList = await FileHelper.LoadPuzzleList(FILEPATH);
-                foreach (var passwordGroup in passwordList)
-                {
-                   
-                }
-                
-                return numberOfValidPasswords;
+                //Right 1, down 1.
+                var numberOfTrees11 = CountNumberOfTrees(1, 1, itineraries);
+                //Right 3, down 1. (This is the slope you already checked.)
+                var numberOfTrees31 = CountNumberOfTrees(3, 1, itineraries);
+                //Right 5, down 1.
+                var numberOfTrees51 = CountNumberOfTrees(5, 1, itineraries);
+                //Right 7, down 1.
+                var numberOfTrees71 = CountNumberOfTrees(7, 1, itineraries);
+                //Right 1, down 2.
+                var numberOfTrees12 = CountNumberOfTrees(1, 2, itineraries);
+
+                return numberOfTrees11*numberOfTrees31*numberOfTrees51*numberOfTrees71*numberOfTrees12;
             }
             catch (Exception ex) 
             {
@@ -49,6 +50,34 @@ namespace AdventOfCode.Puzzles
             }
             
             return -1;
+        }
+
+        private static int CountNumberOfTrees(int slope, int depth, List<string> itineraries) 
+        {
+            var numberOfTrees = 0;
+                var slopeCounter = slope;
+                
+                for(int i = depth; i < itineraries.Count; i+=depth)
+                {
+                    if(slopeCounter >= itineraries[i].Length)
+                    {
+                        slopeCounter = slopeCounter - itineraries[i].Length;
+                    }
+
+                    var isTree = CheckForTree(itineraries[i], slopeCounter);
+                    if(isTree)
+                    {
+                        numberOfTrees++;
+                    }
+                    slopeCounter = slopeCounter+slope;
+                }
+                return numberOfTrees;
+        }
+        
+        public static bool CheckForTree(string itinerary, int slope)
+        {
+            var trajectory = itinerary.Substring(slope, 1);
+            return trajectory.Equals("#");
         }
     }
 }
